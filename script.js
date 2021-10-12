5c47192204696a87d9da86e85a6384494e0878fe
mike-swaney <swaney.mick@gmail.com>
2021-10-12 15:47:50 +1100

    Script complete

--- script.js
+++ script.js
@@ -4,11 +4,37 @@ function generateRandomNumber(num) {
   }
 
 // Create arrays for three sets of data. Arrays work well to randomly access elements using the above funtion.
-const randomlySelected = {
+const randomTriSession = {
   sport: ['swim', 'ride', 'run'],
   trainingSession: ['aerobic', 'tempo', 'V02 max'],
   duration: ['30 min', '60 min', '90 min']
 }
-  
 
+// Create an array that the random variables can be pushed to.
+let personalWorkout = []
+
+// Create a for loop that iterates over randomTriSession.
+for (let thing in randomTriSession) {
+  let optionId = generateRandomNumber(randomTriSession[thing].length)
+  switch (thing) {
+    case 'sport':
+      personalWorkout.push(`Today you will ${randomTriSession[thing][optionId]} `);
+      break;
+    case 'trainingSession':
+      personalWorkout.push(`at ${randomTriSession[thing][optionId]} pace `);
+      break;
+    case 'duration':
+      personalWorkout.push(`for a duration of ${randomTriSession[thing][optionId]}.`);
+      break;
+    default:
+      personalWorkout.push('There is not enough info here');
+      break; 
+  }
+}
+function formatWorkout(session) {
+  const formatted = personalWorkout.join('');
+  console.log(formatted)
+}
+
+formatWorkout(personalWorkout);
  
